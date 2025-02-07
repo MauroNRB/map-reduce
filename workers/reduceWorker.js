0ebb3eb52ded1c1ebbd9aca8fc0108fa024a0643
Mauro Ribeiro <37764309+MauroNRB@users.noreply.github.com>
2025-02-07 09:55:51 -0300

    correção do calculo

--- workers/reduceWorker.js
+++ workers/reduceWorker.js
@@ -1,12 +1,13 @@
 const { parentPort, workerData } = require("worker_threads");
 
 const reducedData = workerData.reduce(
-    (acc, { modelo, velocidade, quantidade }) => {
+    (acc, { modelo, velocidade }) => {
         if (!acc[modelo]) {
             acc[modelo] = { totalVelocidade: 0, totalCarros: 0 };
         }
-        acc[modelo].totalVelocidade += velocidade * quantidade;
-        acc[modelo].totalCarros += quantidade;
+        
+        acc[modelo].totalVelocidade += velocidade;
+        acc[modelo].totalCarros += 1;
         return acc;
     },
     {}
